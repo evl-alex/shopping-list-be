b23ced86ef528315e7317d0035d4617a409123a1
Oleksandr Bibik <obibik@emergemarket.com>
2022-09-20 07:30:05 +0300

    enabled CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ProductsModule } from './products/products.module';
 async function bootstrap() {
   const app = await NestFactory.create(ProductsModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors();
   await app.listen(3001);
 }
 
